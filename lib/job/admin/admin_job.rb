405cca0656b0b5786c01d1671885262b88bae46d
Haopeng Liu <haliu@zendesk.com>
2023-03-21 14:33:19 -0700

    [GJ-9] Admin Job

--- lib/job/admin/admin_job.rb
+++ lib/job/admin/admin_job.rb
@@ -1,2 +1,27 @@
-class AdminJob
+module Job
+  class AdminJob < BaseJob
+    attr_reader :children, :children_generated
+
+    def initialize(id)
+      super(id)
+      @complexity = :admin
+      @children = []
+    end
+
+    def status_report
+      status_hash = {}
+      children.each do |child|
+        status_hash[child.id] = child.done?
+      end
+      status_hash
+    end
+
+    def ready_to_mark_as_done?
+      done = children_generated
+      children.each do |child|
+        done = false unless child.done?
+      end
+      done
+    end
+  end
 end
