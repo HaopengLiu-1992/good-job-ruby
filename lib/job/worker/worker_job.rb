7d432395f979c835b07aaf5b3ea649ceb6a6aef2
Haopeng Liu <haliu@zendesk.com>
2023-03-22 14:34:19 -0700

    [GJ-10] Worker Job

--- lib/job/worker/worker_job.rb
+++ lib/job/worker/worker_job.rb
@@ -1,2 +1,19 @@
-class WorkerJob
+class WorkerJob < BaseJob
+
+  attr_reader :parent_id, :job_type
+
+  def initialize(id, parent_id)
+    super(id)
+    @parent_id = parent_id
+    @complexity = :worker
+  end
+
+  def ready_to_mark_as_done?
+    @control && @control.done?
+  end
+
+  def status_report
+    @control.status
+  end
+
 end
