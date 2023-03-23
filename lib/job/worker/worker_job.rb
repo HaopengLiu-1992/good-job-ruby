020d88197419ff4ae540f4db4ff228a06f6290e5
Haopeng Liu <haliu@zendesk.com>
2023-03-23 14:37:16 -0700

    [GJ-11] Bugfix

--- lib/job/worker/worker_job.rb
+++ lib/job/worker/worker_job.rb
@@ -1,19 +1,22 @@
-class WorkerJob < BaseJob
+module Job
+  class WorkerJob < BaseJob
+    attr_reader :parent_id, :job_type
+    def initialize(id, parent_id)
+      super(id)
+      @parent_id = parent_id
+      @complexity = :worker
+    end
 
-  attr_reader :parent_id, :job_type
+    def ready_to_mark_as_done?
+      self.initialized? && self.done?
+    end
 
-  def initialize(id, parent_id)
-    super(id)
-    @parent_id = parent_id
-    @complexity = :worker
+    def initialized?
+      raise NotImplementedError
+    end
+
+    def status_report
+      self.status
+    end
   end
-
-  def ready_to_mark_as_done?
-    @control && @control.done?
-  end
-
-  def status_report
-    @control.status
-  end
-
 end
