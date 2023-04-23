be5639f79da2c5789577b93128f1618be10fc1bb
Haopeng Liu <haliu@zendesk.com>
2023-04-23 15:15:20 -0700

    [GJ-25] Add job tests

---
+++ test/job/base_job_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+class BaseJobTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+end
